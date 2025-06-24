9be91044fbfb0391480de92932c0f4189f677d8f
newton <wamitinewton@gmail.com>
2025-06-24 13:44:30 +0300

    init

---
+++ src/test/java/com/spring/ai_playground/AiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
